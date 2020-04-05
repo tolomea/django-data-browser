06eeae9bb3bbca559fc36847b90d51c9f117f50e
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-04-05 09:16:45 +0100

    remove tests from built package

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tolomea/django-data-browser",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
