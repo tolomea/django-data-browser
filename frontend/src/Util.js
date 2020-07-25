c89f5d374ad0b67c041110100d2ba383006a3bbc
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-07-25 13:17:10 +0100

    shallowEqual debug helper

--- frontend/src/Util.js
+++ frontend/src/Util.js
@@ -242,6 +242,53 @@ function Overlay(props) {
     );
 }
 
+function is(x: any, y: any) {
+    return (
+        (x === y && (x !== 0 || 1 / x === 1 / y)) || (x !== x && y !== y) // eslint-disable-line no-self-compare
+    );
+}
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+function shallowEqual(objA: mixed, objB: mixed): boolean {
+    if (is(objA, objB)) {
+        return true;
+    }
+
+    if (
+        typeof objA !== "object" ||
+        objA === null ||
+        typeof objB !== "object" ||
+        objB === null
+    ) {
+        return false;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+        console.log("different keys", keysA, keysB);
+        return false;
+    }
+
+    // Test for A's keys different from B.
+    for (let i = 0; i < keysA.length; i++) {
+        if (
+            !hasOwnProperty.call(objB, keysA[i]) ||
+            !is(objA[keysA[i]], objB[keysA[i]])
+        ) {
+            console.log(
+                "different key",
+                keysA[i],
+                objA[keysA[i]],
+                objB[keysA[i]]
+            );
+            return false;
+        }
+    }
+
+    return true;
+}
+
 export {
     TLink,
     SLink,
@@ -256,4 +303,5 @@ export {
     CopyText,
     fetchInProgress,
     Overlay,
+    shallowEqual,
 };
