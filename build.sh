4ab74079490323c2d68fd0f5b4844b384fcf1451
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-11-02 21:45:51 +0000

    2.2.21

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ex
 ./build_fe.sh
 ./build_whl.sh
 
-git add data_browser/fe_build data_browser/templates/data_browser/index.html
+git add data_browser/fe_build data_browser/templates/data_browser/index.html README.rst data_browser/__init__.py
 version=$(python -c "import data_browser; print(data_browser.version)")
 set +x
 echo SUCCESS
