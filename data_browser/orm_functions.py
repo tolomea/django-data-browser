76f90df86e13272398537ddea256d623a95a92ba
Gordon Wrigley <gordon.wrigley@gmail.com>
2023-07-01 15:36:00 +0100

    moving bits around

--- data_browser/orm_functions.py
+++ data_browser/orm_functions.py
@@ -20,32 +20,6 @@ try:
 except ModuleNotFoundError:  # pragma: no cover
     ArrayLenTransform = None
 
-TYPE_FUNCTIONS = {type_: [] for type_ in TYPES.values()}
-
-for type_ in TYPES.values():
-    TYPE_FUNCTIONS[type_].append("is_null")
-
-for array_type in ARRAY_TYPES.values():
-    TYPE_FUNCTIONS[array_type].append("length")
-
-for type_ in [DateType, DateTimeType]:
-    TYPE_FUNCTIONS[type_].append("year")
-    TYPE_FUNCTIONS[type_].append("quarter")
-    TYPE_FUNCTIONS[type_].append("month")
-    TYPE_FUNCTIONS[type_].append("day")
-    TYPE_FUNCTIONS[type_].append("week_day")
-    TYPE_FUNCTIONS[type_].append("month_start")
-    TYPE_FUNCTIONS[type_].append("iso_year")
-    TYPE_FUNCTIONS[type_].append("iso_week")
-    TYPE_FUNCTIONS[type_].append("week_start")
-
-TYPE_FUNCTIONS[DateTimeType].append("hour")
-TYPE_FUNCTIONS[DateTimeType].append("minute")
-TYPE_FUNCTIONS[DateTimeType].append("second")
-TYPE_FUNCTIONS[DateTimeType].append("date")
-
-TYPE_FUNCTIONS[StringType].append("length")
-
 
 _month_choices = [
     (1, "January"),
@@ -74,10 +48,61 @@ _weekday_choices = [
 ]
 
 
-def _get_django_function(type_, name, qs):
-    def IsNull(field_name):
-        return ExpressionWrapper(Q(**{field_name: None}), output_field=BooleanField())
+def IsNull(field_name):
+    return ExpressionWrapper(Q(**{field_name: None}), output_field=BooleanField())
 
+
+class OrmBoundFunctionField(OrmBoundField):
+    def _annotate(self, qs, debug=False):
+        func = _get_django_function(self.previous.type_, self.name, qs)[0](
+            self.previous.queryset_path_str
+        )
+        return self._annotate_qs(qs, func)
+
+    def parse_lookup(self, lookup, value):
+        parsed, error_message = super().parse_lookup(lookup, value)
+        if (
+            self.name in ["year", "iso_year"]
+            and parsed is not None
+            and lookup != "is_null"
+        ):
+            if parsed < 2:
+                error_message = "Can't filter to years less than 2"
+            if parsed > 9998:
+                error_message = "Can't filter to years greater than 9998"
+        return parsed, error_message
+
+
+class OrmFunctionField(OrmBaseField):
+    def __init__(self, base_type, name, type_, choices, default_sort, format_hints):
+        super().__init__(
+            base_type.name,
+            name,
+            name.replace("_", " "),
+            type_=type_,
+            concrete=True,
+            can_pivot=True,
+            choices=choices,
+            default_sort=default_sort,
+            format_hints=format_hints,
+        )
+        self.base_type = base_type
+
+    def bind(self, previous):
+        assert previous
+        assert previous.type_ == self.base_type
+        full_path = previous.full_path + [self.name]
+        return OrmBoundFunctionField(
+            field=self,
+            previous=previous,
+            full_path=full_path,
+            pretty_path=previous.pretty_path + [self.pretty_name],
+            queryset_path=[annotation_path(full_path)],
+            filter_=True,
+        )
+
+
+def _get_django_function(type_, name, qs):
     if issubclass(type_, ArrayTypeMixin) and name == "length":
         return (ArrayLenTransform, NumberType, (), None, {})
 
@@ -106,76 +131,51 @@ def _get_django_function(type_, name, qs):
         "date": (functions.TruncDate, DateType, (), ASC, {}),
         "is_null": (IsNull, IsNullType, (), None, {}),
         "length": (functions.Length, NumberType, (), None, {}),
+        "iso_year": (functions.ExtractIsoYear, NumberType, (), ASC, {}),
+        "iso_week": (functions.ExtractWeek, NumberType, (), ASC, {}),
+        "week_start": (
+            lambda x: functions.TruncWeek(x, DateField()),
+            DateType,
+            (),
+            ASC,
+            {},
+        ),
     }
-    mapping.update(
-        {
-            "iso_year": (functions.ExtractIsoYear, NumberType, (), ASC, {}),
-            "iso_week": (functions.ExtractWeek, NumberType, (), ASC, {}),
-            "week_start": (
-                lambda x: functions.TruncWeek(x, DateField()),
-                DateType,
-                (),
-                ASC,
-                {},
-            ),
-        }
-    )
     return mapping[name]
 
 
-class OrmBoundFunctionField(OrmBoundField):
-    def _annotate(self, qs, debug=False):
-        func = _get_django_function(self.previous.type_, self.name, qs)[0](
-            self.previous.queryset_path_str
-        )
-        return self._annotate_qs(qs, func)
+TYPE_FUNCTIONS = {type_: [] for type_ in TYPES.values()}
 
-    def parse_lookup(self, lookup, value):
-        parsed, error_message = super().parse_lookup(lookup, value)
-        if (
-            self.name in ["year", "iso_year"]
-            and parsed is not None
-            and lookup != "is_null"
-        ):
-            if parsed < 2:
-                error_message = "Can't filter to years less than 2"
-            if parsed > 9998:
-                error_message = "Can't filter to years greater than 9998"
-        return parsed, error_message
+for type_ in TYPES.values():
+    TYPE_FUNCTIONS[type_].append("is_null")
 
+for array_type in ARRAY_TYPES.values():
+    TYPE_FUNCTIONS[array_type].append("length")
 
-class OrmFunctionField(OrmBaseField):
-    def __init__(self, model_name, name, type_, choices, default_sort, format_hints):
-        super().__init__(
-            model_name,
-            name,
-            name.replace("_", " "),
-            type_=type_,
-            concrete=True,
-            can_pivot=True,
-            choices=choices,
-            default_sort=default_sort,
-            format_hints=format_hints,
-        )
+for type_ in [DateType, DateTimeType]:
+    TYPE_FUNCTIONS[type_].append("year")
+    TYPE_FUNCTIONS[type_].append("quarter")
+    TYPE_FUNCTIONS[type_].append("month")
+    TYPE_FUNCTIONS[type_].append("day")
+    TYPE_FUNCTIONS[type_].append("week_day")
+    TYPE_FUNCTIONS[type_].append("month_start")
+    TYPE_FUNCTIONS[type_].append("iso_year")
+    TYPE_FUNCTIONS[type_].append("iso_week")
+    TYPE_FUNCTIONS[type_].append("week_start")
 
-    def bind(self, previous):
-        assert previous
-        full_path = previous.full_path + [self.name]
-        return OrmBoundFunctionField(
-            field=self,
-            previous=previous,
-            full_path=full_path,
-            pretty_path=previous.pretty_path + [self.pretty_name],
-            queryset_path=[annotation_path(full_path)],
-            filter_=True,
-        )
+TYPE_FUNCTIONS[DateTimeType].append("hour")
+TYPE_FUNCTIONS[DateTimeType].append("minute")
+TYPE_FUNCTIONS[DateTimeType].append("second")
+TYPE_FUNCTIONS[DateTimeType].append("date")
+
+TYPE_FUNCTIONS[StringType].append("length")
 
 
 def get_functions_for_type(type_):
     funcs = TYPE_FUNCTIONS[type_]
     return {
         func: OrmFunctionField(
-            type_.name, func, *_get_django_function(type_, func, None)[1:]
+            type_, func, *_get_django_function(type_, func, None)[1:]
         )
         for func in funcs
     }
