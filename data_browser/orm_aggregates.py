0bdd4a1f5966cabe487ece6b8acf598a678ac085
Gordon Wrigley <gordon.wrigley@gmail.com>
2023-07-01 12:08:01 +0100

    merge funcs into type aggregates

--- data_browser/orm_aggregates.py
+++ data_browser/orm_aggregates.py
@@ -48,38 +48,59 @@ TYPE_AGGREGATES = {type_: [] for type_ in TYPES.values()}
 
 for type_ in TYPES.values():
     if type_ != BooleanType:
-        TYPE_AGGREGATES[type_].append(("count", NumberType))
+        TYPE_AGGREGATES[type_].append(
+            ("count", lambda x: models.Count(x, distinct=True), NumberType)
+        )
 
 TYPE_AGGREGATES[NumberType].extend(
     [
-        ("average", NumberType),
-        ("max", NumberType),
-        ("min", NumberType),
-        ("std_dev", NumberType),
-        ("sum", NumberType),
-        ("variance", NumberType),
+        ("average", models.Avg, NumberType),
+        ("max", models.Max, NumberType),
+        ("min", models.Min, NumberType),
+        ("std_dev", models.StdDev, NumberType),
+        ("sum", models.Sum, NumberType),
+        ("variance", models.Variance, NumberType),
     ]
 )
 
-TYPE_AGGREGATES[DateTimeType].extend([("max", DateTimeType), ("min", DateTimeType)])
+TYPE_AGGREGATES[DateTimeType].extend(
+    [("max", models.Max, DateTimeType), ("min", models.Min, DateTimeType)]
+)
 
-TYPE_AGGREGATES[DateType].extend([("max", DateType), ("min", DateType)])
+TYPE_AGGREGATES[DateType].extend(
+    [("max", models.Max, DateType), ("min", models.Min, DateType)]
+)
 
 TYPE_AGGREGATES[DurationType].extend(
     [
-        ("average", DurationType),
-        ("max", DurationType),
-        ("min", DurationType),
-        ("sum", DurationType),
+        ("average", lambda x: models.Avg(_CastDuration(x)), DurationType),
+        ("max", models.Max, DurationType),
+        ("min", models.Min, DurationType),
+        ("sum", lambda x: models.Sum(_CastDuration(x)), DurationType),
     ]
 )
 
-TYPE_AGGREGATES[BooleanType].extend([("average", NumberType), ("sum", NumberType)])
+TYPE_AGGREGATES[BooleanType].extend(
+    [
+        (
+            "average",
+            lambda x: models.Avg(Cast(x, output_field=IntegerField())),
+            NumberType,
+        ),
+        ("sum", lambda x: models.Sum(Cast(x, output_field=IntegerField())), NumberType),
+    ]
+)
 
 if "postgresql" in settings.DATABASES["default"]["ENGINE"]:
     for array_type in ArrayTypeMixin.__subclasses__():
         if array_type.raw_type is None:
-            TYPE_AGGREGATES[array_type.element_type].append(("all", array_type))
+            TYPE_AGGREGATES[array_type.element_type].append(
+                (
+                    "all",
+                    lambda x: ArrayAgg(x, default=Value([]), distinct=True, ordering=x),
+                    array_type,
+                )
+            )
 
 
 class _CastDuration(Cast):
@@ -92,36 +113,8 @@ class _CastDuration(Cast):
         return self.as_sql(compiler, connection, template=template, **extra_context)
 
 
-def _get_django_aggregate(field_type, name):
-    if name == "all":
-        return lambda x: ArrayAgg(x, default=Value([]), distinct=True, ordering=x)
-
-    if field_type == BooleanType:
-        return {
-            "average": lambda x: models.Avg(Cast(x, output_field=IntegerField())),
-            "sum": lambda x: models.Sum(Cast(x, output_field=IntegerField())),
-        }[name]
-    if field_type == DurationType and name in ["average", "sum"]:
-        return {
-            "average": lambda x: models.Avg(_CastDuration(x)),
-            "sum": lambda x: models.Sum(_CastDuration(x)),
-        }[name]
-    else:
-        return {
-            "average": models.Avg,
-            "count": lambda x: models.Count(x, distinct=True),
-            "max": models.Max,
-            "min": models.Min,
-            "std_dev": models.StdDev,
-            "sum": models.Sum,
-            "variance": models.Variance,
-        }[name]
-
-
 def get_aggregates_for_type(type_):
     return {
-        aggregate: OrmAggregateField(
-            type_, aggregate, _get_django_aggregate(type_, aggregate), res_type
-        )
-        for aggregate, res_type in TYPE_AGGREGATES[type_]
+        aggregate: OrmAggregateField(type_, aggregate, func, res_type)
+        for aggregate, func, res_type in TYPE_AGGREGATES[type_]
     }
