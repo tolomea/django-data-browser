d09c2ad1504f87ffce8f16552f5f58f9c87150ff
Azeem <azeemanimashaun@gmail.com>
2024-10-22 13:26:14 +0100

    Add edX plugin support

--- data_browser/apps.py
+++ data_browser/apps.py
@@ -6,3 +6,23 @@ class DataBrowserConfig(AppConfig):
     name = "data_browser"
     verbose_name = "Data Browser"
     default_auto_field = AutoField
+
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': '/admin/data-browser/',
+                'regex': r'^data_browser/',
+                'relative_path': 'urls',
+            },
+            'cms.djangoapp': {
+                'namespace': '/admin/data-browser/',
+                'regex': r'^data_browser/',
+                'relative_path': 'urls',
+            }
+        },
+    }
+
+    def ready(self):
+        from django.conf import settings
+        if 'data_browser' not in settings.INSTALLED_APPS:
+            settings.INSTALLED_APPS += ('data_browser',)
